41ea3aacfcbc6e1188b60c9ab137adf54f741b01
gouravbeesayti <gbeesayti@gmail.com>
2024-02-23 20:21:15 +0530

    reactjs

---
+++ src/components/ProductList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useFilterContext } from '../context/filter_context'
+import GridView from './GridView'
+
+const ProductList = () => {
+  const {filter_products,setGridView}=useFilterContext()
+  // console.log(filter_products)
+
+  
+    return <GridView products ={filter_products}/>
+  
+  // if(setGridView === false)
+  // {
+  //   return <ListView products={filter_products} />
+  // }
+  
+}
+
+export default ProductList
